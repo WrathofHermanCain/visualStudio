58676a4e9f0e75035dd631a4ed8d68a70cb54227
Liam McKnight <lmcknight.sf@gmail.com>
2025-08-07 21:59:46 -0400

    Add random greeting generator function

--- visualStudio.py
+++ visualStudio.py
@@ -4701,3 +4701,19 @@ if x%2 == 0:
 else:
     print("x is odd")
 
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+print(get_random_greeting())
+print("Learning about branches today!")
+
